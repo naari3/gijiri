f47674f81516019f90faccbbec2a8c35e741e508
naari3 <naari.named@gmail.com>
2021-09-30 20:48:36 +0900

    Setup

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    // 型を必要としないプラグインの推奨ルールをすべて有効
+    'plugin:@typescript-eslint/recommended',
+    // 型を必要とするプラグインの推奨ルールをすべて有効
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }],
+    'import/prefer-default-export': 'off',
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+  },
+};
