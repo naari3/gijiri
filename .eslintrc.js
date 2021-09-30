27db97051f6a24adddf1e906388d7a3a4eff6636
naari3 <naari.named@gmail.com>
2021-09-30 22:18:37 +0900

    Implement recorder https://github.com/discordjs/voice/tree/main/examples/recorder

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     'import/extensions': ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }],
     'import/prefer-default-export': 'off',
     'newline-before-return': 'error',
-    'no-console': 'warn',
+    'no-console': 'off',
     'no-var': 'error',
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log('helloworld');
+/* eslint-disable no-console */
+import Discord, { Interaction } from 'discord.js';
+import { getVoiceConnection } from '@discordjs/voice';
+import { deploy } from './deploy';
+import { interactionHandlers } from './interactions';
+
+const client = new Discord.Client({ intents: ['GUILD_VOICE_STATES', 'GUILD_MESSAGES', 'GUILDS'] });
+
+client.on('ready', () => console.log('Ready'));
+
+client.on('messageCreate', async (message) => {
+  if (!message.guild) return;
+  if (!client.application?.owner) await client.application?.fetch();
+
+  if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner?.id) {
+    await deploy(message.guild);
+    await message.reply('Deployed!');
+  }
+});
+
+/**
+ * The IDs of the users that can be recorded by the bot.
+ */
+const recordable = new Set<string>();
+
+client.on('interactionCreate', async (interaction: Interaction) => {
+  if (!interaction.isCommand() || !interaction.guildId) return;
+
+  const handler = interactionHandlers.get(interaction.commandName);
+
+  try {
+    if (handler) {
+      await handler(interaction, recordable, client, getVoiceConnection(interaction.guildId));
+    } else {
+      await interaction.reply('Unknown command');
+    }
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+client.on('error', console.warn);
+
+// eslint-disable-next-line no-void
+void client.login(process.env.TOKEN);
