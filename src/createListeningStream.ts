8e320d7e9bca48774ec1ccc66b7fb6eef388b7bd
naari3 <naari.named@gmail.com>
2021-09-30 23:35:14 +0900

    Implement connect to speech to text

--- src/createListeningStream.ts
+++ src/createListeningStream.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { EndBehaviorType, VoiceReceiver } from '@discordjs/voice';
 import { User } from 'discord.js';
-import { createWriteStream } from 'fs';
+// import { createWriteStream } from 'fs';
 import { opus } from 'prism-media';
 import { pipeline } from 'stream';
+import { SpeechClient } from '@google-cloud/speech';
 
 function getDisplayName(userId: string, user?: User) {
   return user ? `${user.username}_${user.discriminator}` : userId;
@@ -28,15 +31,37 @@ export function createListeningStream(receiver: VoiceReceiver, userId: string, u
 
   const filename = `./recordings/${Date.now()}-${getDisplayName(userId, user)}.ogg`;
 
-  const out = createWriteStream(filename);
+  // const out = createWriteStream(filename);
 
-  console.log(`👂 Started recording ${filename}`);
+  // console.log(`👂 Started recording ${filename}`);
 
-  pipeline(opusStream, oggStream, out, (err) => {
-    if (err) {
-      console.warn(`❌ Error recording file ${filename} - ${err.message}`);
-    } else {
-      console.log(`✅ Recorded ${filename}`);
-    }
+  const client = new SpeechClient();
+  const recognizeStream = client
+    .streamingRecognize({
+      config: {
+        encoding: 'OGG_OPUS',
+        sampleRateHertz: 48000,
+        audioChannelCount: 2,
+        languageCode: 'ja-JP',
+        model: 'phone_call',
+      },
+      interimResults: false,
+    })
+    .on('error', console.error)
+    .on('data', (data) =>
+      process.stdout.write(
+        data.results[0] && data.results[0].alternatives[0]
+          ? `${user?.username}: ${data.results[0].alternatives[0].transcript}\n`
+          : '\n\nReached transcription time limit, press Ctrl+C\n'
+      )
+    );
+
+  // pipeline(opusStream, oggStream, recognizeStream, (err) => {
+  pipeline(opusStream, oggStream, recognizeStream, (err) => {
+    // if (err) {
+    //   console.warn(`❌ Error recording file ${filename} - ${err.message}`);
+    // } else {
+    //   console.log(`✅ Recorded ${filename}`);
+    // }
   });
 }
